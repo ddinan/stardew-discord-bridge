8130ef0b00ccfb3417cf8d5d60c7b6393e96997d
Derek <19647340+ddinan@users.noreply.github.com>
2023-03-08 16:37:34 +1000

    Upload files

---
+++ DiscordBot.cs
@@ -0,0 +1,103 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DiscordBridge;
+using Newtonsoft.Json;
+using StardewModdingAPI;
+using System.IO;
+using System.Threading.Tasks;
+
+public class DiscordBot
+{
+    public static Task Start() => new DiscordBot().MainAsync();
+
+    private static DiscordSocketClient client;
+
+    public class DiscordConfig
+    {
+        public string token;
+        public ulong channelID;
+    }
+
+    public async Task MainAsync()
+    {
+        if (!File.Exists(Path.Combine(ModEntry.path, "config.json")))
+        {
+            ModEntry.ModMonitor.Log("The file 'config.json' was not found.", LogLevel.Error);
+            return;
+        }
+
+        var token = JsonConvert.DeserializeObject<DiscordConfig>(File.ReadAllText(Path.Combine(ModEntry.path, "config.json"))).token;
+
+        //ModEntry.SendChatMessage(token + " token");
+
+        var config = new DiscordSocketConfig()
+        {
+            // We need to allow Discord intents to read member messages
+            GatewayIntents = GatewayIntents.All
+        };
+
+        client = new DiscordSocketClient(config);
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        // Add handlers
+        client.Log += Log;
+        client.MessageReceived += MessageReceived;
+
+        client.Ready += () =>
+        {
+            ModEntry.ModMonitor.Log("Discord bot is now running.", LogLevel.Info);
+            return Task.CompletedTask;
+        };
+
+        await Task.Delay(-1);
+    }
+
+    private static async Task MessageReceived(SocketMessage message)
+    {
+        var channel = message.Channel as SocketGuildChannel;
+        if (channel == null) return;
+
+        if (message.Author.IsBot) return; // Don't relay bot messages
+
+        if (!File.Exists(Path.Combine(ModEntry.path, "config.json")))
+        {
+            ModEntry.ModMonitor.Log("The file 'config.json' was not found.", LogLevel.Error);
+            return;
+        }
+
+        var channelID = JsonConvert.DeserializeObject<DiscordConfig>(File.ReadAllText(Path.Combine(ModEntry.path, "config.json"))).channelID;
+
+        if (channel.Id != channelID) return; // Only send in specific channel
+
+        if (message.Content.ToLower().StartsWith(".who"))
+        {
+            // TODO: Show players online
+            return;
+        }
+
+        ModEntry.SendChatMessage(message.Author.ToString() + ": " + message.Content);
+    }
+
+    public static async Task SendChannelMessageAsync(string message)
+    {
+        if (!File.Exists(Path.Combine(ModEntry.path, "config.json")))
+        {
+            ModEntry.ModMonitor.Log("The file 'config.json' was not found.", LogLevel.Error);
+            return;
+        }
+
+        ulong channelID = JsonConvert.DeserializeObject<DiscordConfig>(File.ReadAllText(Path.Combine(ModEntry.path, "config.json"))).channelID;
+
+        var channel = await client.GetChannelAsync(channelID) as IMessageChannel;
+        if (channel == null) return;
+
+        await channel!.SendMessageAsync(message);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ ModEntry.cs
@@ -0,0 +1,70 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace DiscordBridge
+{
+    /// <summary>The mod entry point.</summary>
+    class ModEntry : Mod
+    {
+        /// <summary>Provides method to log to the console.</summary>
+        public static IMonitor ModMonitor;
+
+        /// <summary>
+        /// The path of the mod folder.
+        /// </summary>
+        public static string path = "";
+
+        /// <summary>The mod entry point</summary>
+        /// <param name="helper">Provides methods for interacting with the mod directory as well as the modding API.</param>
+        public override void Entry(IModHelper helper)
+        {
+            // Update the path
+            path = Helper.DirectoryPath;
+
+            // Create a new Harmony instance for patching the source code
+            var harmony = new Harmony(ModManifest.UniqueID);
+
+            // Override the default receiveChatMessage() behaviour
+            harmony.Patch(
+               original: AccessTools.Method(typeof(ChatBox), nameof(ChatBox.receiveChatMessage)),
+               postfix: new HarmonyMethod(AccessTools.Method(typeof(ModEntry), nameof(ModEntry.ReceiveChatMessageHandler)))
+            );
+
+            ModMonitor = Monitor;
+
+            DiscordBot.Start(); // Start the Discord bot
+        }
+
+        public static void SendChatMessage(string message)
+        {
+            Game1.chatBox.addMessage("Discord: " + message, new Microsoft.Xna.Framework.Color(128, 0, 128)); // RGB 128, 0, 128 = purple
+        }
+
+        /// <summary>Custom handler to retrieve messages sent to chat.</summary>
+        /// <param name="sourceFarmer">The player that sent the message.</param>
+        /// <param name="message">The message content.</param>
+        private static async void ReceiveChatMessageHandler(long sourceFarmer, int chatKind, LocalizedContentManager.LanguageCode language, string message)
+        {
+            string playerName = "";
+
+            // Find the player's name from the ID
+            if (sourceFarmer == Game1.player.UniqueMultiplayerID)
+            {
+                playerName = ChatBox.formattedUserName(Game1.player);
+            }
+
+            else if (Game1.otherFarmers.ContainsKey(sourceFarmer))
+            {
+                playerName = ChatBox.formattedUserName(Game1.otherFarmers[sourceFarmer]);
+            }
+
+            // Only show messages sent by players
+            if (playerName != "")
+            {
+                await DiscordBot.SendChannelMessageAsync("**" + playerName + "**: " + message); // Send the message on Discord
+            }
+        }
+    }
+}
